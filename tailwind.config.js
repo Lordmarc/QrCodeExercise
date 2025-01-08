e441302a22fc2ce0ad1e0fab05f8b2e37516b62c
lordmarc <matabanglordmarcsiega@gmail.com>
2025-01-08 18:21:32 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./design/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      outfit: 'Outfit',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
